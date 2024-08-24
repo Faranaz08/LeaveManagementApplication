77b8e852454015f7d5f2c2a8565432539b7d8e69
Faranaz08 <faranazannigeri3142@gmail.com>
2024-08-25 00:11:15 +0530

    Repo and service class added

---
+++ Employee/src/main/java/com/fara/Employee/repo/EmployeeRepo.java
@@ -0,0 +1,23 @@
+package com.fara.Employee.repo;
+
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+import com.fara.Employee.pojo.EmployeePojo;
+
+@Repository
+public class EmployeeRepo{
+	 private List<EmployeePojo> employees = new ArrayList<>();
+
+	    public List<EmployeePojo> findAll() {
+	        return employees;
+	    }
+
+	    public void addEmployee(EmployeePojo employee) {
+	        employees.add(employee);
+	    }
+
+	    public EmployeePojo findById(String id) {
+	        return employees.stream().filter(emp -> emp.getId().equals(id)).findFirst().orElse(null);
+	    }
+}
---
+++ Employee/src/main/java/com/fara/Employee/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.fara.Employee.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fara.Employee.pojo.EmployeePojo;
+import com.fara.Employee.repo.EmployeeRepo;
+
+import java.util.List;
+
+@Service
+public class EmployeeService{
+	@Autowired
+    private EmployeeRepo employeeRepository;
+
+    public List<EmployeePojo> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void addEmployee(EmployeePojo employee) {
+        employeeRepository.addEmployee(employee);
+    }
+
+    public EmployeePojo getEmployeeById(String id) {
+        return employeeRepository.findById(id);
+    }
+}

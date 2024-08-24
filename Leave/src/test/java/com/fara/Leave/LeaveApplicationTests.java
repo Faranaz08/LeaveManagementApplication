8b6ee04f789eb95aa47121c24a142781a9022eb0
Faranaz08 <faranazannigeri3142@gmail.com>
2024-08-24 22:56:49 +0530

    Imports of Spring Initilizer

---
+++ Leave/src/test/java/com/fara/Leave/LeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fara.Leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
